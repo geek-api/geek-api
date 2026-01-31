035b531eddcc087b4e54fa71c9a7242545f08679
juliolmuller <juliolmuller@outlook.com>
2026-01-31 09:50:26 -0300

    chore: configure ESlint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,17 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import anyConfig from 'eslint-config-any';
+import nextTsConfig from 'eslint-config-next/typescript';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
+export default defineConfig([
+  ...anyConfig.react,
+  ...nextTsConfig,
   globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'next-env.d.ts',
+    'node_modules/**',
+    'build/**',
+    'dist/**',
+    'out/**',
+    'README.md',
   ]),
 ]);
-
-export default eslintConfig;
